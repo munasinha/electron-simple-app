84d232f7898933996375c1fda540ad59a92ac079
Navod_Rashmika <navod199736@gmail.com>
2019-07-16 11:04:19 +0530

    install axias  and impliment target price show in index window

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
-
+const axios = require('axios');
+const ipc = electron.ipcRenderer
 
 const notifyBtn = document.getElementById('notifyBtn')
 
+var price = document.querySelector('h1');
+var targetPrice = document.getElementById('targetPrice')
+
+function getBTC(){
+    axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
+            .then(res => {
+                const cryptos = res.data.USD
+                price.innerHTML = '$'+ cryptos.toLocaleString('en');
+               console.log(cryptos)
+            })
+}
+
+getBTC();
+setInterval(getBTC, 30000);
+
 notifyBtn.addEventListener('click', function (event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
-    let win = new BrowserWindow({ width: 400, height: 200 })
+    let win = new BrowserWindow({ frame : false, transparent: true, alwaysOnTop:true, width: 400, height: 200 })
     win.on('close', function () { win = null })
     win.loadURL(modalPath)
     win.show()
   })
+
+
+  ipc.on('targetPriceVal', function (event, arg) {
+    targetPriceVal = Number(arg);
+    targetPrice.innerHTML = '$'+targetPriceVal.toLocaleString('en')
+})
